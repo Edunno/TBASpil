92c338057dfb717578c167e48578ec1ce0239078
caspe <caspe@DESKTOP-AB8L9P6.local>
2018-04-10 15:37:55 +0200

    New class - Actions 1.7.14

---
+++ src/tbaspil/Actions.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tbaspil;
+import Rooms.Room;
+/**
+ *
+ * @author caspe
+ */
+public class Actions {
+    
+    public void Action(String actionTaken, Room currentRoom){
+        
+    }
+}
