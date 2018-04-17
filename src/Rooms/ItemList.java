50c8af2cb876b9ecca48e5e76e4556338bcac614
caspe <caspe@DESKTOP-AB8L9P6.local>
2018-04-17 12:48:27 +0200

    ItemList new items 2.1.3.6

--- src/Rooms/ItemList.java
+++ src/Rooms/ItemList.java
@@ -16,12 +16,45 @@ public class ItemList {
     
     
     public Item getWoodSword(){
-        Item woodSword = new Item("Wooden Sword","A wooden sword. It's pretty scrappy, and pretty crappy, but I guess it beats having to use your fists.",2,0,0,0,0,true,true);
+        Item woodSword = new Item("Wooden Sword","A wooden sword. It's pretty scrappy, and pretty crappy, but I guess it beats having to use your fists.",true);
+        //Set item values
         return woodSword;
     }
     
-    public Item momsDildo(){
-        Item momsDildo = new Item("Moms Dildo","It's moist, dripping and you can feel the warmth of your mothers loom",3,0,5,2,0,true,true);
+    public Item getMomsDildo(){
+        Item momsDildo = new Item("Moms Dildo","It's moist, dripping and you can feel the warmth of your mothers loom",true);
+        //Set item values
         return momsDildo;
     }
+    
+    public Item getMorningStar(){
+        Item morningStar = new Item("Morning Star","A morning star. Dont swing it headless around. You could end headless.",true);
+        //Set item values
+        return morningStar;
+    }
+    
+    public Item getDaddysBelt(){
+        Item daddysBelt = new Item("Daddys Belt","Oh boi, you know this one hurts like fuck.",true);
+        //Set item values
+        return daddysBelt;
+    }
+    
+    public Item getGoose(){
+        Item goose = new Item("Goose","Legendary weapon. It literally can break your arm.",true);
+        //Set item values
+        return goose;
+    }
+    
+    public Item getMonsterEnergy(){
+        Item monsterEnergy = new Item("Monster Energy","Programming will be easier now, but you don't have a pc",true);
+        //Set item values
+        return monsterEnergy;
+    }
+    
+    public Item getPlasticShield(){
+        Item plasticShield = new Item("Plastic Shield","Not very effective and harms the inviroment. *SCREECH* YOU MURDER", true);
+        //Set item values
+        return plasticShield;
+    }
+    
 }
