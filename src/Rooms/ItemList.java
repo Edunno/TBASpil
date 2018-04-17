ff66965bc1fcb6f24edd097330acf09faefd95f5
caspe <caspe@DESKTOP-AB8L9P6.local>
2018-04-17 15:42:37 +0200

    ItemList rollback 2.1.3.8

--- src/Rooms/ItemList.java
+++ src/Rooms/ItemList.java
@@ -21,40 +21,9 @@ public class ItemList {
         return woodSword;
     }
     
-    public Item getMomsDildo(){
+    public Item momsDildo(){
         Item momsDildo = new Item("Moms Dildo","It's moist, dripping and you can feel the warmth of your mothers loom",true);
         //Set item values
         return momsDildo;
     }
-    
-    public Item getMorningStar(){
-        Item morningStar = new Item("Morning Star","A morning star. Dont swing it headless around. You could end headless.",true);
-        //Set item values
-        return morningStar;
-    }
-    
-    public Item getDaddysBelt(){
-        Item daddysBelt = new Item("Daddys Belt","Oh boi, you know this one hurts like fuck.",true);
-        //Set item values
-        return daddysBelt;
-    }
-    
-    public Item getGoose(){
-        Item goose = new Item("Goose","Legendary weapon. It literally can break your arm.",true);
-        //Set item values
-        return goose;
-    }
-    
-    public Item getMonsterEnergy(){
-        Item monsterEnergy = new Item("Monster Energy","Programming will be easier now, but you don't have a pc",true);
-        //Set item values
-        return monsterEnergy;
-    }
-    
-    public Item getPlasticShield(){
-        Item plasticShield = new Item("Plastic Shield","Not very effective and harms the inviroment. *SCREECH* YOU MURDER", true);
-        //Set item values
-        return plasticShield;
-    }
-    //test
 }
