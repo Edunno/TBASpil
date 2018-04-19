359ab51fc3885d82b194c864b0dc65e8093c2ee4
KimPPedersen <KimHotDK@github.com>
2018-04-19 12:12:16 +0200

    Austin Powers & Babysitter 2.9.7.18

--- src/Player/Monsters/Babysitter.java
+++ src/Player/Monsters/Babysitter.java
@@ -17,13 +17,15 @@ public class Babysitter extends Monster {
 
     private String voiceAttack;
     private int dmg1 = 0;
+    private int dmg2 = 0;
     private Random r = new Random();
     private ArrayList attackOptions;
     int rand;
+    
 
     public Babysitter() {
-        super(10, 1);
-        attackOptions.add("Attack");
+        super(10, 0);
+        attackOptions.add("attack");
         attackOptions.add("Yell at babysitter");
         attackOptions.add("Snarky comeback");
         attackOptions.add("Start crying");
@@ -42,29 +44,29 @@ public class Babysitter extends Monster {
         switch (rand) {
             case 1:
                 voiceAttack = "You go to bed, young man";
-                dmg1 = 1;
+                dmg1 = dmg2 +1;
                 break;
             case 2:
                 voiceAttack = "You're not old enough to look at those, young man";
-                dmg1 = 0;
+                dmg1 = dmg2 + 0;
                 break;
             case 3:
                 voiceAttack = "I'm telling your parents, young man! You're going to be so much in trouble!";
-                dmg1 = 2;
+                dmg1 = dmg2 + 2;
                 break;
             case 4:
                 voiceAttack = "Don't you roll your eyes at me, young man";
-                dmg1 = 1;
+                dmg1 = dmg2 + 1;
                 attackOptions.remove("Snarky comeback");
-                attackOptions.add("Repeat in in a high pitch voice what babysitter said");
+                attackOptions.add("Repeat what babysitter said");
                 break;
             case 5:
                 voiceAttack = "I'm going to count to three and then you're in trouble, young man";
-                dmg1 = 0;
+                dmg1 = dmg2 + 0;
                 break;
             case 6:
                 voiceAttack = "Aren't you cuttest one, young man";
-                dmg1 = 0;
+                dmg1 = dm2 + 0;
                 attackOptions.remove("attack");
                 attackOptions.add("Make out with babysitter");
                 break;
@@ -79,7 +81,7 @@ public class Babysitter extends Monster {
         switch (a) {
             case "attack":
                 takeDamage(0);
-                dmg1 = 3;
+                dmg2 = 3;
                 break;
             case "yell at babysitter":
                 takeDamage(1);
@@ -89,7 +91,7 @@ public class Babysitter extends Monster {
                 break;
             case "start crying":
                 takeDamage(0);
-                dmg1 = -2;
+                dmg2 = -2;
                 break;
             default:
                 break;
@@ -100,18 +102,18 @@ public class Babysitter extends Monster {
         switch (a) {
             case "attack":
                 takeDamage(0);
-                dmg1 = 3;
+                dmg2 = 3;
                 break;
             case "yell at babysitter":
                 takeDamage(1);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             case "Snarky comeback":
                 takeDamage(3);
                 break;
             case "start crying":
                 takeDamage(0);
-                dmg1 = 2;
+                dmg2 = 2;
                 break;
             default:
                 break;
@@ -122,19 +124,19 @@ public class Babysitter extends Monster {
         switch (a) {
             case "attack":
                 takeDamage(1);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             case "yell at babysitter":
                 takeDamage(1);
-                dmg1 = 1;
+                dmg2 = 1;
                 break;
             case "Snarky comeback":
                 takeDamage(3);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             case "start crying":
                 takeDamage(2);
-                dmg1 = 2;
+                dmg2 = 2;
                 break;
             default:
                 break;
@@ -145,19 +147,19 @@ public class Babysitter extends Monster {
         switch (a) {
             case "attack":
                 takeDamage(2);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             case "yell at babysitter":
                 takeDamage(2);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
-            case "Repeat in in a high pitch voice what babysitter said":
+            case "Repeat what babysitter said":
                 takeDamage(3);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             case "start crying":
                 takeDamage(0);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             default:
                 break;
@@ -168,19 +170,19 @@ public class Babysitter extends Monster {
         switch (a) {
             case "attack":
                 takeDamage(2);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             case "yell at babysitter":
                 takeDamage(3);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             case "Snarky comeback":
                 takeDamage(5);
-                dmg1 = 0;
+                dmg2 = 0;
                 break;
             case "start crying":
                 takeDamage(0);
-                dmg1 = 2;
+                dmg2 = 2;
                 break;
             default:
                 break;
@@ -190,16 +192,16 @@ public class Babysitter extends Monster {
             case 6:
                 if (a.equals("Make out with babysitter")) {
                     takeDamage(10);
-                    dmg1 = 0;
+                    dmg2 = 0;
                 } else if (a.equals("yell at babysitter")) {
                     takeDamage(2);
-                    dmg1 = 0;
+                    dmg2 = 0;
                 } else if (a.equals("Snarky comeback")) {
                     takeDamage(3);
-                    dmg1 = 0;
+                    dmg2 = 0;
                 } else if (a.equals("start crying")) {
                     takeDamage(2);
-                    dmg1 = 5;
+                    dmg2 = 5;
                 }
                 break;
         }
